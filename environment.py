fbdb0f5417cf5c42e3ff39236924092fd7349df5
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-12-26 14:35:47 +0200

    basic environment skeleton

---
+++ environment.py
@@ -0,0 +1,35 @@
+from kubernetes import client, config
+from constants import CASSANDRA_STATEFULSET_NAME, NAMESPACE
+from clients import Clients
+
+
+class KubernetesEnv:
+    def __init__(self):
+        # Load Kubernetes configuration
+        config.load_kube_config()
+
+        # Create API clients
+        v1 = client.CoreV1Api()
+        apps_v1 = client.AppsV1Api()
+        autoscaling_v2 = client.AutoscalingV2Api()
+
+        clients = Clients(v1, apps_v1, autoscaling_v2)
+
+        self.statefulset_name = CASSANDRA_STATEFULSET_NAME
+        self.namespace = NAMESPACE
+        self.state = self.get_state()
+        self.clients = clients
+
+    def reset(self):
+        pass
+
+    def get_state(self):
+        apps_v1 = self.clients.apps_v1
+        statefulset = apps_v1.read_namespaced_stateful_set(
+            self.statefulset_name, self.namespace)
+        print(statefulset)
+
+
+if __name__ == "__main__":
+    environment = KubernetesEnv()
+    print(environment)
