000236428710fce6383540ede07c3c7ef021ef2c
Geo Angelopoulos <geo.angelotti@gmail.com>
2024-12-26 19:43:37 +0200

    cql constants

--- constants.py
+++ constants.py
@@ -1,3 +1,18 @@
 CASSANDRA_STATEFULSET_NAME = "cassandra"
 
 NAMESPACE = "default"
+
+CREATE_KEYSPACE = "create keyspace ycsb WITH REPLICATION = {'class' : 'SimpleStrategy', 'replication_factor': 3 };"
+USE_KEYSPACE = "use ycsb;"
+CREATE_TABLE = """create table usertable (
+y_id varchar primary key,
+field0 varchar,
+field1 varchar,
+field2 varchar,
+field3 varchar,
+field4 varchar,
+field5 varchar,
+field6 varchar,
+field7 varchar,
+field8 varchar,
+field9 varchar);"""
