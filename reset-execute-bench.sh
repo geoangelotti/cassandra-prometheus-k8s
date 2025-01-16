76b26e9b526772e08dc985d44af7e66b1cf2d0e2
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-01-16 23:13:38 +0100

    get command argument

--- reset-execute-bench.sh
+++ reset-execute-bench.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --hpa)
+            HPA="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown parameter: $1"
+            exit 1
+            ;;
+    esac
+done
+
 python3 /home/ubuntu/cassandra-prometheus-k8s/environment.py
 sleep 15
 /home/ubuntu/ycsb/bin/ycsb load cassandra-cql -p hosts=localhost -s -P workloads/workloadaConstantLoad
