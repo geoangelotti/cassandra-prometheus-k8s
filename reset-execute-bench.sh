06f18b0d62f8b2d454dd1b12bd471e316ec1ce24
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-01-16 23:16:42 +0100

    handle hpa argument

--- reset-execute-bench.sh
+++ reset-execute-bench.sh
@@ -17,6 +17,33 @@ python3 /home/ubuntu/cassandra-prometheus-k8s/environment.py
 sleep 15
 /home/ubuntu/ycsb/bin/ycsb load cassandra-cql -p hosts=localhost -s -P workloads/workloadaConstantLoad
 sleep 3m
+
+case $HPA in
+    "cpu")
+        echo "Configuring CPU-based HPA"
+        # Your CPU HPA command here
+        kubectl apply -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-cpu.yaml
+        ;;
+    "mem")
+        echo "Configuring Memory-based HPA"
+        # Your Memory HPA command here
+        kubectl apply -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-mem.yaml
+        ;;
+    "both")
+        echo "Configuring both CPU and Memory HPA"
+        # Commands for both
+        kubectl apply -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa.yaml
+        ;;
+    "")
+        echo "No HPA type specified. Use --hpa with cpu, mem, or both"
+		echo "Starting dry"
+        ;;
+    *)
+        echo "Invalid HPA type. Use cpu, mem, or both"
+        exit 1
+        ;;
+esac
+
 date "+%Y-%m-%d %H:%M:%S"
 /home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=localhost -s -P workloads/workloadaSineLoad -p period=300000
 date "+%Y-%m-%d %H:%M:%S"
