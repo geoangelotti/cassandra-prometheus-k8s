57f4a4c06a35c179a5bd9da8726cdfdb9a1657eb
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-01-19 17:13:52 +0000

    mark use as default workloads as possible

--- reset-execute-bench.sh
+++ reset-execute-bench.sh
@@ -16,7 +16,7 @@ done
 python3 /home/ubuntu/cassandra-prometheus-k8s/environment.py
 sleep 15
 cd /home/ubuntu/ycsb || exit
-/home/ubuntu/ycsb/bin/ycsb load cassandra-cql -p hosts=localhost -s -P workloads/workloadaConstantLoad -p recordcount=5000
+/home/ubuntu/ycsb/bin/ycsb load cassandra-cql -p hosts=localhost -s -P workloads/workloadaConstant
 sleep 3m
 
 case $HPA in
@@ -46,5 +46,5 @@ case $HPA in
 esac
 
 date "+%Y-%m-%d %H:%M:%S"
-/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=localhost -s -P workloads/workloadaSineLoad -p period=300000 -p recordcount=5000 -p operationcount=400000
+/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=localhost -s -P workloads/workloadaSine -p period=300000
 date "+%Y-%m-%d %H:%M:%S"
