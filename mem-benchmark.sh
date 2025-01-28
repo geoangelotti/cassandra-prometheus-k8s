af68caa7ceaf6b87f9ca856159415b46c2002fab
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-01-29 00:45:37 +0100

    try mem benchmark runner

---
+++ mem-benchmark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CASSANDRA_HOSTS=$(kubectl get pods -l app=cassandra -o jsonpath='{.items[*].status.podIP}' | tr ' ' ',')
+CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S" | tr ' ' '_')
+THREADS=500
+OPERATIONCOUNT=20000000
+RECORDCOUNT=100000
+
+kubectl apply -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-cpu.yaml
+sleep 10
+cd /home/ubuntu/ycsb || exit
+/home/ubuntu/ycsb/bin/ycsb load cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaConstant -threads ${THREADS} -p recordcount=${RECORDCOUNT} > ./${CURRENT_TIME}/load.log
+kubectl exec -i -t cassandra-0 -c cassandta -- nodetool status > ./${CURRENT_TIME}/after_load_status.log
+sleep 30
+/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaConstant -threads ${THREADS} -p recordcount=${RECORDCOUNT -p operationcount=${OPERATIONCOUNT}> ./${CURRENT_TIME}/run.log
+kubectl exec -i -t cassandra-0 -c cassandta -- nodetool status > ./${CURRENT_TIME}/after_run_status.log
+kubectl delete -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-cpu.yaml
