b9b4f4e05fbf5fa5c54bacca16836264364a09ba
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-02-04 18:51:37 +0100

    add sine benchmark runner

--- mem-benchmark.sh
+++ mem-benchmark.sh
@@ -5,6 +5,7 @@ CURRENT_TIME=$(date +"%Y-%m-%d_%H:%M:%S")
 THREADS=500
 OPERATIONCOUNT=50000000
 RECORDCOUNT=100000
+PERIOD=1000
 
 kubectl apply -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-cpu.yaml
 sleep 10
@@ -14,6 +15,7 @@ mkdir -p ./${CURRENT_TIME}
 kubectl exec -it cassandra-0 -c cassandra -- nodetool status | tee ./${CURRENT_TIME}/after_load_status.log
 sleep 30
 /home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaConstant -threads ${THREADS} -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} | tee ./${CURRENT_TIME}/run.log
+#/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaSine -threads ${THREADS} -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} -p period=${PERIOD} | tee ./${CURRENT_TIME}/run.log
 kubectl exec -it cassandra-0 -c cassandra -- nodetool status | tee ./${CURRENT_TIME}/after_run_status.log
 kubectl delete -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-cpu.yaml
 kubectl get pods --all-namespaces -o wide -l app=cassandra | tee ./${CURRENT_TIME}/cassandra_pods.log
