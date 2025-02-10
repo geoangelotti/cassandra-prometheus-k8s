6fb150e84ea516d72ae3b033361e467ba6600853
Geo Angelopoulos <geo.angelotti@gmail.com>
2025-02-10 22:32:11 +0100

    edit sine benchmarks

--- sine-benchmark.sh
+++ sine-benchmark.sh
@@ -2,8 +2,8 @@
 
 CASSANDRA_HOSTS=$(kubectl get pods -l app=cassandra -o jsonpath='{.items[*].status.podIP}' | tr ' ' ',')
 CURRENT_TIME=$(date +"%Y-%m-%d_%H:%M:%S")
-THREADS=500
-OPERATIONCOUNT=50000000
+THREADS=15
+OPERATIONCOUNT=5000000
 RECORDCOUNT=100000
 PERIOD=1000
 
@@ -14,8 +14,7 @@ mkdir -p ./sine/${CURRENT_TIME}
 /home/ubuntu/ycsb/bin/ycsb load cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaConstant -threads ${THREADS} -p recordcount=${RECORDCOUNT} | tee ./${CURRENT_TIME}/load.log
 kubectl exec -it cassandra-0 -c cassandra -- nodetool status | tee ./${CURRENT_TIME}/after_load_status.log
 sleep 30
-/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaConstant -threads ${THREADS} -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} | tee ./${CURRENT_TIME}/run.log
-#/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaSine -threads ${THREADS} -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} -p period=${PERIOD} | tee ./${CURRENT_TIME}/run.log
+/home/ubuntu/ycsb/bin/ycsb run cassandra-cql -p hosts=${CASSANDRA_HOSTS} -s -P workloads/workloadaSine -threads ${THREADS} -p recordcount=${RECORDCOUNT} -p operationcount=${OPERATIONCOUNT} -p period=${PERIOD} -p amplitude=150 -p baseTarget=200 -p strategy=sine | tee ./${CURRENT_TIME}/run.log
 kubectl exec -it cassandra-0 -c cassandra -- nodetool status | tee ./${CURRENT_TIME}/after_run_status.log
 kubectl delete -f /home/ubuntu/cassandra-prometheus-k8s/manifests/cassandra-hpa-cpu.yaml
 kubectl get pods --all-namespaces -o wide -l app=cassandra | tee ./${CURRENT_TIME}/cassandra_pods.log
